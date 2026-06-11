3f634f259e7bbf3a8d88ef890277974d33429d3e
delsonlife <44551767+delsonlife@users.noreply.github.com>
2026-06-11 22:29:28 +0100

    Update build.js

--- build.js
+++ build.js
@@ -1,10 +1,7 @@
 const JavaScriptObfuscator = require('javascript-obfuscator');
 const fs = require('fs');
 
-// Lire votre widget actuel
 const code = fs.readFileSync('public/widget.js', 'utf8');
-
-// Obfusquer
 const result = JavaScriptObfuscator.obfuscate(code, {
   compact: true,
   controlFlowFlattening: true,
@@ -13,7 +10,5 @@ const result = JavaScriptObfuscator.obfuscate(code, {
   stringArray: true,
   stringArrayEncoding: ['rc4']
 });
-
-// Créer la version obfusquée
 fs.writeFileSync('public/widget.min.js', result.getObfuscatedCode());
 console.log('✅ widget.min.js généré');
